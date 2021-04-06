1a5401aad72639bbf5b236577aad1c3cbe586764
sayutii <76469871+sayutii@users.noreply.github.com>
2021-04-06 23:18:48 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<!-- Created By CodingNepal -->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CV - Muhammad Sayuti</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"/>
+
+</head>
+<body>
+    <div class="scroll-up-btn">
+        <i class="fas fa-angle-up"></i>
+    </div>
+    <nav class="navbar">
+        <div class="max-width">
+            <div class="logo"><a href="#"><span>Sayutii</span></a></div>
+            <ul class="menu">
+                <li><a href="#home" class="menu-btn">Beranda</a></li>
+                <li><a href="#about" class="menu-btn">Tentang</a></li>
+                <li><a href="#skills" class="menu-btn">Kemampuan</a></li>
+                <li><a href="#teams" class="menu-btn">Tim</a></li>
+                <li><a href="#contact" class="menu-btn">Kontak</a></li>
+                <li><a href="https://sayutiii.wordpress.com/" class="menu-btn">WordPress</a></li>
+                <li><a href="https://sayuti.sayutii.repl.co" class="menu-btn">Replit</a></li>
+            </ul>
+            <div class="menu-btn">
+                <i class="fas fa-bars"></i>
+            </div>
+        </div>
+    </nav>
+
+    <!-- home section start -->
+    <section class="home" id="home">
+      <div class="max-width">
+          <div class="row">
+            <div class="home-content">
+                <div class="text-1">MUHAMMAD SAYUTI</div>
+                <div class="text-2">Banda Aceh, Indonesia </div>
+                <a href="#">Hubungi Saya</a>
+            </div>
+          </div>
+      </div>
+    </section>
+
+    <!-- about section start -->
+    <section class="about" id="about">
+        <div class="max-width">
+            <h2 class="title">Tentang Saya</h2>
+            <div class="about-content">
+                <div class="column left">
+                    <img src="images/yut.jpg" alt="">
+                </div>
+                <div class="column right">
+                    <div class="text">Muhammad Sayuti</div>
+                    <p>Izinkan saya memperkenalkan diri, nama saya Muhammad Sayuti biasa dipanggil sayuti, beragama islam dan berkebangkasaan Indonesia. Saya lahir di desa Teupin Keubeu pada tanggal 30 maret 2000, anak terakhir dari empat bersaudara. Saya adalah seorang mahasiswa dari Politeknik Aceh dengan program study Teknologi Informasi.</p>
+                 </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- services section start -->
+    <section class="services" id="services">
+        <div class="max-width">
+            <div class="serv-content">
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-school"></i>
+                        <div class="text">SD Negeri 8 Matangkuli</div>
+                        <p>2006 - 2012</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-school"></i>
+                        <div class="text">SMP Negeri 1 Matangkuli</div>
+                        <p>2012 - 2015</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <i class="fas fa-school"></i>
+                        <div class="text">SMA Negeri 1 Matangkuli</div>
+                        <p>2015 - 2018</p>
+                    </div>
+                </div>
+               </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- skills section start -->
+    <section class="skills" id="skills">
+        <div class="max-width">
+            <h2 class="title">Kemampuan Saya</h2>
+            <div class="skills-content">
+                <div class="column left">
+                    <div class="text">Multimedia</div>
+                    <p>Adobe Photoshop, Adobe Ilustrator, HeavyM.</p>
+                </div>
+                <div class="column right">
+                    <div class="bars">
+                        <div class="info">
+                            <span>HTML</span>
+                            <span>85%</span>
+                        </div>
+                        <div class="line html"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>CSS</span>
+                            <span>65%</span>
+                        </div>
+                        <div class="line css"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>JavaScript</span>
+                            <span>45%</span>
+                        </div>
+                        <div class="line js"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>PHP</span>
+                            <span>60%</span>
+                        </div>
+                        <div class="line php"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>MySQL</span>
+                            <span>80%</span>
+                        </div>
+                        <div class="line mysql"></div>
+                    </div>
+                    <div class="bars">
+                        <div class="info">
+                            <span>Android</span>
+                            <span>30%</span>
+                        </div>
+                        <div class="line android"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- teams section start -->
+    <section class="teams" id="teams">
+        <div class="max-width">
+            <h2 class="title">Claws Four</h2>
+            <div class="carousel owl-carousel">
+                <div class="card">
+                    <div class="box">
+                        <img src="images/haha.jpeg" alt="">
+                        <div class="text">C4 <span>• </span>Deago</div>
+                        <p>Sayuti biasa dipanggil deago Master sniper, dan sangat barbar.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/sodok.jpeg" alt="">
+                        <div class="text">C4 <span>• </span>Zonk</div>
+                        <p>farhan biasa dipangil sodok Suka toxic, main nub, dan buaya.</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="box">
+                        <img src="images/hasan.JPG" alt="">
+                        <div class="text">C4 <span>• </span>Cing</div>
+                        <p>Hasanul biasa dipanggil cingyt mainnya hebat, lumayan jago dikit.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- contact section start -->
+    <section class="contact" id="contact">
+        <div class="max-width">
+            <h2 class="title">Kontak Saya</h2>
+            <div class="contact-content">
+                <div class="column left">
+                    <div class="text">Gak Pakek Sosial Media</div>
+                    <p>Anda sopan kami curiga, jangan-jangan mau minjam duit.</p>
+                    <div class="icons">
+                        <div class="row">
+                            <i class="fas fa-user"></i>
+                            <div class="info">
+                                <div class="head">Nama</div>
+                                <div class="sub-title">Muhammad Sayuti</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-map-marker-alt"></i>
+                            <div class="info">
+                                <div class="head">Alamat</div>
+                                <div class="sub-title">Banda Aceh, Indonesia</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-envelope"></i>
+                            <div class="info">
+                                <div class="head">Email</div>
+                                <div class="sub-title">msayuti903@gmail.com</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column right">
+                    <div class="text">Pesan</div>
+                    <form class="contact-form" action="#" method="POST">
+                        <div class="fields">
+                            <div class="field name">
+                                <input type="text" class="fullname" placeholder="Nama">
+                            </div>
+                            <div class="field email">
+                                <input type="text" class="email-input" placeholder="Email">
+                            </div>
+                        </div>
+                        <div class="field">
+                            <input type="text" class="subject" placeholder="Subjek">
+                        </div>
+                        <div class="field textarea">
+                            <textarea class="message" cols="30" rows="10" placeholder="Pesan.."></textarea>
+                        </div>
+                        <div class="button-area">
+                            <button class="send-msg" type="submit" name="send">Kirim</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- footer section start -->
+    <footer>
+        <span>Created By <a href="https://www.instagram.com">sayutii</a> | <span class="far fa-copyright"></span> 2021 All rights reserved.</span>
+    </footer>
+
+    <script src="script.js"></script>
+</body>
+</html>
